580150bdc6a5c4d9f8c3385924ecde96e9513950
ly <384823211@qq.com>
2019-08-03 08:47:14 +0800

    'update'

---
+++ codeCommit.js
@@ -0,0 +1,9 @@
+
+/**命令行代码提交方式**/
+
+1.git checkout 分支名(如 feature/mybranch)     (切换到指定开发分支)
+2.git pull    (拉取代码)
+3.git add .    (暂存需要提交的文件)
+4.git commit -m '备注'    (提交已暂存的文件)
+5.git push    (同步到服务器)
+

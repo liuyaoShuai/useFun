f98bbe1ab5f7911e420b114553bdca98c9e85f44
liuyaoShuai <384823211@qq.com>
2019-10-18 09:23:23 +0800

    create uuid

--- oftenFun.js
+++ oftenFun.js
@@ -1016,3 +1016,17 @@ export class DownloadService {
     }
 }
 
+// create uuid
+ getUUID() {
+        return 'xxxx-xxxx-xxxx-xxxx'.replace(/[xy]/g, function(c) {
+            let r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        });
+    }
+    getUUID2() {
+        return 'xxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            let r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        });
+    }
+
